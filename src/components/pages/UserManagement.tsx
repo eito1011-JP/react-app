c8618d83dfd85f72fa6a487cca07ca09bfb11ec0
諸橋瑛音 <eito@8.local>
2024-02-25 20:29:48 +0900

    final

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -44,7 +44,7 @@ export const UserManagement: FC = memo(() => {
                 ))}
             </Wrap>
         )}        
-        <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose}/>
+        <UserDetailModal user={selectedUser} isOpen={isOpen} isAdmin={loginUser?.isAdmin} onClose={onClose}/>
         </>
     );
 });
--- src/components/pages/organisms/user/UserDetailModal.tsx
+++ src/components/pages/organisms/user/UserDetailModal.tsx
@@ -1,40 +1,68 @@
-import { FC, memo } from "react";
-import { Modal, ModalOverlay, ModalContent, useDisclosure, ModalHeader, ModalCloseButton, ModalBody, Stack, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { ChangeEvent, FC, memo, useEffect, useState } from "react";
+import { Modal, ModalOverlay, ModalContent, ModalFooter, ModalHeader, ModalCloseButton, ModalBody, Stack, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { User } from "../../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 type Props = {
     user: User | null,
     isOpen: boolean,
-    onClose: () => void
+    onClose: () => void,
+    isAdmin?: boolean,
 };
 
-export const UserDetailModal: FC<Props> = memo(({ user, isOpen, onClose }) => {
+export const UserDetailModal: FC<Props> = memo(({ user, isOpen, onClose, isAdmin = false }) => {
+    const onClickUpdate = () => alert();
+    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    useEffect(() => {
+        setUsername(user?.username ?? '')
+        setName(user?.name ?? '')
+        setEmail(user?.email ?? '')
+        setPhone(user?.phone ?? '')
+    }, [user]);
+
+    const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+
+    const onChangeName = (e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+
+    const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+
+    const onChangePhone = (e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value);
+
     return (
         <Modal isOpen={isOpen} onClose={onClose} autoFocus={false} motionPreset="slideInBottom">
             <ModalOverlay>
-                <ModalContent pb={6}>
+                <ModalContent pb={2}>
                     <ModalHeader>ユーザー詳細</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody mx={4}>
                         <Stack spacing={4}>
                             <FormControl>
                                 <FormLabel>名前</FormLabel>
-                                <Input value={user?.username || ''} isReadOnly/>
+                                <Input value={username} onChange={onChangeName} isReadOnly={!isAdmin}/>
                             </FormControl>
                             <FormControl>
                                 <FormLabel>フルネーム</FormLabel>
-                                <Input value={user?.name || ''} isReadOnly/>
+                                <Input value={name} onChange={onChangeUserName} isReadOnly={!isAdmin}/>
                             </FormControl>
                             <FormControl>
                                 <FormLabel>Mail</FormLabel>
-                                <Input value={user?.email || ''} isReadOnly/>
+                                <Input value={email} onChange={onChangeEmail} isReadOnly={!isAdmin}/>
                             </FormControl>
                             <FormControl>
                                 <FormLabel>電話</FormLabel>
-                                <Input value={user?.phone || ''} isReadOnly/>
+                                <Input value={phone} onChange={onChangePhone} isReadOnly={!isAdmin}/>
                             </FormControl>
                         </Stack>
                     </ModalBody>
+                    {isAdmin && (
+                    <ModalFooter>
+                        <PrimaryButton onClick={onClickUpdate}>更新</PrimaryButton>
+                    </ModalFooter>
+                    )}
                 </ModalContent>
             </ModalOverlay>
         </Modal>
